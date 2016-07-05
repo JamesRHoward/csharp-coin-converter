fe11de143daa311425189644400960eb07a3ea2e
James Howard and Wolfgang Warneke <james+wolfgang@>
2016-07-05 16:52:37 -0700

    dimes are all good now

--- Objects/Coin.cs
+++ Objects/Coin.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+
 
 namespace Coin
 {
@@ -11,12 +13,22 @@ namespace Coin
       changeDictionary.Add("dimes", 0);
       changeDictionary.Add("nickels", 0);
       changeDictionary.Add("pennies", 0);
-      if ( changeTotal % 1 == 0) {
-        return changeDictionary;
-      } else {
-        System.Console.WriteLine("You have failed");
+      double runningRemainingTotal = changeTotal;
+      this.PrintCoinAmounts(changeDictionary, runningRemainingTotal);
+      if (changeTotal % 1 == 0) {
         return changeDictionary;
       }
+      changeDictionary["quarters"] = Convert.ToInt32(Math.Floor(runningRemainingTotal/0.25));
+      runningRemainingTotal = runningRemainingTotal % 0.25;
+      this.PrintCoinAmounts(changeDictionary, runningRemainingTotal);
+      changeDictionary["dimes"] = Convert.ToInt32(Math.Ceiling(runningRemainingTotal/0.10));
+      runningRemainingTotal = runningRemainingTotal % 0.10;
+      this.PrintCoinAmounts(changeDictionary, runningRemainingTotal);
+      return changeDictionary;
+    }
+    public void PrintCoinAmounts(Dictionary<string, int> currentCoins, double runningTotal)
+    {
+      Console.WriteLine(currentCoins["quarters"] + " " + currentCoins["dimes"] + " " + currentCoins["nickels"] + " " + currentCoins["pennies"] + " and " + runningTotal + " remaining");
     }
   }
 }
--- Tests/TestCoin.cs
+++ Tests/TestCoin.cs
@@ -5,16 +5,40 @@ namespace Coin
 {
   public class CoinTest
   {
+    // [Fact]
+    // public void CalculateChange_ForAmountDivisibleByWholeDollar_NoCoins()
+    // {
+    //   CalculateChange testCalculateChange = new CalculateChange();
+    //   Dictionary<string, int> testDictionary = new Dictionary<string, int>();
+    //   testDictionary.Add("quarters", 0);
+    //   testDictionary.Add("dimes", 0);
+    //   testDictionary.Add("nickels", 0);
+    //   testDictionary.Add("pennies", 0);
+    //   Assert.Equal(testDictionary, testCalculateChange.ReturnCoins(12));
+    // }
+    //
+    // [Fact]
+    // public void CalculateChange_ForRemainingAmountDivisibleBy25Cents_Quarters()
+    // {
+    //   CalculateChange testCalculateChange = new CalculateChange();
+    //   Dictionary<string, int> testDictionary = new Dictionary<string, int>();
+    //   testDictionary.Add("quarters", 3);
+    //   testDictionary.Add("dimes", 0);
+    //   testDictionary.Add("nickels", 0);
+    //   testDictionary.Add("pennies", 0);
+    //   Assert.Equal(testDictionary, testCalculateChange.ReturnCoins(0.75));
+    // }
+
     [Fact]
-    public void CalculateChange_ForAmountDivisibleByWholeDollar_NoCoins()
+    public void CalculateChange_ForRemainingAmountDivisibleBy10Cents_Dimes()
     {
       CalculateChange testCalculateChange = new CalculateChange();
       Dictionary<string, int> testDictionary = new Dictionary<string, int>();
-      testDictionary.Add("quarters", 0);
-      testDictionary.Add("dimes", 0);
+      testDictionary.Add("quarters", 3);
+      testDictionary.Add("dimes", 2);
       testDictionary.Add("nickels", 0);
       testDictionary.Add("pennies", 0);
-      Assert.Equal(testDictionary, testCalculateChange.ReturnCoins(12));
+      Assert.Equal(testDictionary, testCalculateChange.ReturnCoins(0.95));
     }
 
 
